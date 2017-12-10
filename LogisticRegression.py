ba2c3482541bce39647e989fd64812fc7f64a0c3
bean <bean@beande-MacBook-Pro.local>
2017-12-10 14:29:38 +0800

    fix bugs in Logistic Regression

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -8,31 +8,43 @@ from scipy.special import expit
 #Logistic Regression class
 class LogisticRegression(object):
     #initialize
-    def __init__(self,epochs=500,eta=0.001, shuffle=True,random_state=None):
+    def __init__(self,epochs=500,eta=0.001, shuffle=True,random_state=None,earlystop=150, L2=0.001):
         np.random.seed(random_state)
         self.epochs = epochs
         self.eta = eta
         self.shuffle = shuffle
         self.w_initialized=False
-
-    def fit(self, X, y):
+        self.earlystop=earlystop
+        self.L2=L2
+        
+    def fit(self, X, y, weightinit=False):
         #X:array-like shape=[n_samples, n_features], trainging vectors
         #y:array-like shape=[n_samples], means true label value
         self.n_features=X.shape[1]
+        self.w_initialized=weightinit
         if (self.w_initialized==False):
             self.w_=self._initialize_weights()
         self.error_=[]
+        self.stopstep
+        self.stopstep = 0
         #loop for training
         for i_trai in range(self.epochs):
+            if self.stopstep >=self.earlystop:
+                print("Stop at Epoch {}, E_in is {}".format(i_trai+1,self.error_[-1]))
+                break
             #data shuffle
             if self.shuffle:
                 X,y = self._shuffle(X,y)
 
             self.error_.append(self._get_error(X,y))
+            #set early stop here                                                              
+            if i_trai>50:
+                if (self.error_[-1]>self.error_[-2]):
+                    self.stopstep+=1
+            
             self._update_weights(X,y)
 
         return self.error_
-
             
     #initialize the weighting vector with random number (-1,1)
     #dimension of weighting vector is n_features+1 (w0....wd)
@@ -49,33 +61,26 @@ class LogisticRegression(object):
     #update the weighting vector
     def _update_weights(self,X,y):
         n_samples=X.shape[0]
-        wx_=self.net_input(X)
-        zi=[]
-        for wxi, yi in zip(wx_,y):
-            zi.append(-wxi*yi)
         #put in sigmoid function result
-        net_sig=self._sigmoid(np.array(zi))
-        net_yx=[]
-        net_y=[]
-        for Xi, yi, sigi in zip(X,y, net_sig):
-            net_yx.append(-yi*Xi*sigi)
-            net_y.append(-yi*sigi)
-
-        net_yx = np.array(net_yx)
-        net_y  = np.array(net_y)
-        self.w_[1:] += self.eta*(-1.0/n_samples)*np.sum(net_yx,axis=0)
-        self.w_[0] += self.eta*(-1.0/n_samples)*np.sum(net_y)
+        wx_=self._sigmoid(self.net_input(X))
         
     
-    #obtain the Ein for each step
+        net_sig = np.subtract(y,wx_)
+        
+        gra = -2.0*np.dot(np.transpose(X),net_sig)+(2.0*self.L2*self.w_[1:])
+        grab = -2.0*np.sum(net_sig)
+        
+        self.w_[1:] += self.eta*(-1.0/n_samples)*gra
+        self.w_[0] += self.eta*(-1.0/n_samples)*grab
+        
+    
+    #obtain the Ein for each step (MSE)
     def _get_error(self, X, y):
         n_samples=X.shape[0]
         wx_=self.net_input(X)
-        Ein_p=[]
-        for wxi, yi in zip(wx_,y):
-            Ein_p.append(np.log(1+np.exp(-wxi*yi)))
-            
+        Ein_p=np.square(np.subtract(wx_,y))
         return (1.0/n_samples)*np.sum(np.array(Ein_p))
+
 
     #return wx
     def net_input(self, X):
